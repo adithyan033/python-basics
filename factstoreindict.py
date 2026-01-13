ad8f6eca782791be77ab43d77e0431f7c708897d
Adithyan <adithya03n@gmail.com>
2026-01-13 18:59:32 +0530

    calculate factorials of numbers between a range an store it in dictionary

---
+++ factstoreindict.py
@@ -0,0 +1,17 @@
+
+dictt=[]
+
+def fact(n):
+    if n==0:
+        return 1
+    else:
+        return n*fact(n-1)
+    
+n1=int(input("enter the starting range "))
+n2=int(input("enter the ending range"))
+    
+for num in range(n1,n2):
+    dictt[num]=fact(num)
+    
+    
+print(dictt)
