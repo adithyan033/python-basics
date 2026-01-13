6a4b49cccde06ccfb64d63b5a613ef9336c7aa2e
Adithyan <adithya03n@gmail.com>
2026-01-13 19:39:33 +0530

    concatenate three separate dictionary into one

---
+++ concatedictionary.py
@@ -0,0 +1,8 @@
+dict1={"Name":"anu","dpt":"cs","gender":"male"}
+dict2={"Name2":"nanu","dpt2":"it","gender2":"trans"}
+dict3={"Name3":"sree","dpt3":"mba","gender3":"female"}
+dict_final={}
+dict_final.update(dict1)
+dict_final.update(dict2)
+dict_final.update(dict3)
+print(dict_final)
